b1616129daa491032938839ef6d1a0f40a076be6
Unknown <adobrinin@gmail.com>
2017-06-01 18:47:25 -0700

    Added RedirectMethodAttribute
    * Identifies methods for redirection.

---
+++ Redirection/RedirectMethodAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace InvertXMouse.Redirection
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RedirectMethodAttribute : Attribute
+    {
+        public bool OnCreated { get; }
+
+
+        public RedirectMethodAttribute()
+        {
+            OnCreated = false;
+        }
+
+        public RedirectMethodAttribute(bool onCreated)
+        {
+            OnCreated = onCreated;
+        }
+    }
+}
