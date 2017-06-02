b14baf2c2160f5109d797321e8a790e441f2a365
Unknown <adobrinin@gmail.com>
2017-06-01 23:04:14 -0700

    Added very temporary InvertXMouseOption
    * This will not be a permanent emplacement. It will be covered by a proper setting in game.

--- InvertXMouse.cs
+++ InvertXMouse.cs
@@ -5,6 +5,8 @@ namespace InvertXMouse
 {
     public class InvertXMouse : IUserMod
     {
+        public static bool InvertXMouseOption = true;
+
         public string Name => "Invert X Mouse";
         public string Description => "Adds the missing Invert X option for the camera.";
     }
