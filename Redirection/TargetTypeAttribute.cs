7ab753e475c3736fc786544181c7c6a54fa88647
Unknown <adobrinin@gmail.com>
2017-06-01 18:44:22 -0700

    Added TargetTypeAttribute
    * Used to identify redirection target classes/structs.

---
+++ Redirection/TargetTypeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace InvertXMouse.Redirection
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class TargetTypeAttribute : Attribute
+    {
+        public Type Type { get; }
+
+
+        public TargetTypeAttribute(Type type)
+        {
+            Type = type;
+        }
+    }
+}
