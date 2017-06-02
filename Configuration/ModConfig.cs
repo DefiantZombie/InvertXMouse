31bcb24cee0dca5bbd416480bdfa0be276ec4460
DefiantZombie <adobrinin@gmail.com>
2017-06-02 02:15:30 -0700

    Added logging and configuration
    * Swapped out cheap disk logger for panel logger.
    * Added configuration classes.

---
+++ Configuration/ModConfig.cs
@@ -0,0 +1,15 @@
+﻿using SexyFishHorse.CitiesSkylines.Infrastructure.Configuration;
+
+
+namespace InvertXMouse.Configuration
+{
+    public static class ModConfig
+    {
+        private static IConfigurationManager _instance;
+
+        public static IConfigurationManager Instance
+        {
+            get { return _instance ?? (_instance = ConfigurationManager.Create(InvertXMouse.ModName)); }
+        }
+    }
+}
